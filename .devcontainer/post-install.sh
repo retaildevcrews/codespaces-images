8079be62c0dfc821f07d5c14b705f4c297b60ff0
Bart Robertson <bartr@microsoft.com>
2021-02-17 00:13:54 +0000

    updated setup

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 apt-get update
 apt-get install -y apt-transport-https gnupg2 curl git ca-certificates lsb-release build-essential httpie nano
-apt-get autoremove -y
 
 cd ~
+echo "k9s" >> status
 curl -Lo ./k9s.tar.gz https://github.com/derailed/k9s/releases/download/v0.24.2/k9s_Linux_x86_64.tar.gz
 mkdir k9s
 tar xvzf k9s.tar.gz -C ./k9s
@@ -11,12 +11,13 @@ mv ./k9s/k9s /usr/bin/k9s
 rm ./k9s.tar.gz
 rm -rf k9s
 
+echo "kind" >> status
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.10.0/kind-linux-amd64
 chmod +x ./kind
 mv ./kind /usr/bin/kind
 
+echo "clone" >> status
 git clone https://github.com/retaildevcrews/ngsa
-
 
 # update .bashrc
 echo "" >> .bashrc
@@ -35,3 +36,5 @@ echo 'export PIP=$(ipconfig | tail -n 1)' >> .bashrc
 echo 'source /usr/share/bash-completion/bash_completion' >> .bashrc
 echo 'source <(kubectl completion bash)' >> .bashrc
 echo 'complete -F __start_kubectl k' >> .bashrc
+
+echo "done" >> status
