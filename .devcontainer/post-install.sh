48cb4a0a736fd3b694c3e966e13031cd4f2bbb6b
bartr <bartr@microsoft.com>
2021-02-21 16:44:26 -0600

    updated setup

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -4,6 +4,35 @@
 mkdir -p ~/.local
 cp .devcontainer/kubectl_completion ~/.local/kubectl_completion
 
+# update .bashrc with helpful aliases
+pushd ~
+echo "" >> .bashrc
+echo "export PATH=$PATH:$HOME/.local/bin" >> .bashrc
+
+echo "alias k='kubectl'" >> .bashrc
+echo "alias kga='kubectl get all'" >> .bashrc
+echo "alias kgaa='kubectl get all --all-namespaces'" >> .bashrc
+echo "alias kaf='kubectl apply -f'" >> .bashrc
+echo "alias kdelf='kubectl delete -f'" >> .bashrc
+echo "alias kl='kubectl logs'" >> .bashrc
+echo "alias kccc='kubectl config current-context'" >> .bashrc
+echo "alias kcgc='kubectl config get-contexts'" >> .bashrc
+
+echo "export GO111MODULE=on" >> .bashrc
+echo "alias ipconfig='ip -4 a show eth0 | grep inet | sed \"s/inet//g\" | sed \"s/ //g\" | cut -d / -f 1'" >> .bashrc
+echo 'export PIP=$(ipconfig | tail -n 1)' >> .bashrc
+echo 'source $HOME/.local/kubectl_completion' >> .bashrc
+echo 'complete -F __start_kubectl k' >> .bashrc
+
+export PATH=$PATH:$HOME/.local/bin
+
+popd
+
+
+### Application specific configuration
+### Delete if reusing for other projects
+
+
 cp .devcontainer/workspace ../akdc.code-workspace
 
 # clone repos
@@ -42,25 +71,3 @@ sudo chown -R 65534:65534 /prometheus
 sudo mkdir -p /grafana
 sudo  cp grafanadata/grafana.db /grafana
 sudo  chown -R 472:472 /grafana
-
-# update .bashrc
-cd ~
-echo "" >> .bashrc
-echo "export PATH=$PATH:$HOME/.local/bin" >> .bashrc
-
-echo "alias k='kubectl'" >> .bashrc
-echo "alias kga='kubectl get all'" >> .bashrc
-echo "alias kgaa='kubectl get all --all-namespaces'" >> .bashrc
-echo "alias kaf='kubectl apply -f'" >> .bashrc
-echo "alias kdelf='kubectl delete -f'" >> .bashrc
-echo "alias kl='kubectl logs'" >> .bashrc
-echo "alias kccc='kubectl config current-context'" >> .bashrc
-echo "alias kcgc='kubectl config get-contexts'" >> .bashrc
-
-echo "export GO111MODULE=on" >> .bashrc
-echo "alias ipconfig='ip -4 a show eth0 | grep inet | sed \"s/inet//g\" | sed \"s/ //g\" | cut -d / -f 1'" >> .bashrc
-echo 'export PIP=$(ipconfig | tail -n 1)' >> .bashrc
-echo 'source $HOME/.local/kubectl_completion' >> .bashrc
-echo 'complete -F __start_kubectl k' >> .bashrc
-
-export PATH=$PATH:$HOME/.local/bin
