e014bf71e1951d6a09c68110ac0a1ce3bb28396b
Bart Robertson <bartr@microsoft.com>
2021-03-03 05:48:10 +0000

    added kdbg alias

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -17,6 +17,7 @@ echo "alias kdelf='kubectl delete -f'" >> .bashrc
 echo "alias kl='kubectl logs'" >> .bashrc
 echo "alias kccc='kubectl config current-context'" >> .bashrc
 echo "alias kcgc='kubectl config get-contexts'" >> .bashrc
+echo "alias kdbg='kubectl exec -it debug -- '" >> .bashrc
 
 echo "export GO111MODULE=on" >> .bashrc
 echo "alias ipconfig='ip -4 a show eth0 | grep inet | sed \"s/inet//g\" | sed \"s/ //g\" | cut -d / -f 1'" >> .bashrc
